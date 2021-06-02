237df1eab8a23a47cd7785bc0ef4e46244abb8e3
paceval <info@paceval.com>
2021-06-02 23:44:13 +0200

    Add files via upload

--- examples_sources/PHP_examples/example1.php
+++ examples_sources/PHP_examples/example1.php
@@ -3,15 +3,15 @@
 // PHP example 1
 
 $paceval_ffi = FFI::cdef("
-bool pacevalLibrary_Initialize(const char* initString_in);
-bool pacevalLibrary_Free();
-void* pacevalLibrary_CreateComputation(const char* functionString_in, unsigned long numberOfVariables_in,
-const char* variables_in, bool useInterval_in, void* paceval_callbackStatus_in);
-double pacevalLibrary_dGetComputationResult(void* handle_pacevalComputation_in, double values_in[],
-double* trustedMinResult_out, double* trustedMaxResult_out);
-bool pacevalLibrary_DeleteComputation(void* handle_pacevalComputation_in);
-",
-"/usr/bin/libpacevalARM64_sharedLIB.so"); // you can exchange the ARM64-library, e.g. to the x64-library
+     bool pacevalLibrary_Initialize(const char* initString_in);
+     bool pacevalLibrary_Free();
+     void* pacevalLibrary_CreateComputation(const char* functionString_in, unsigned long numberOfVariables_in,
+     const char* variables_in, bool useInterval_in, void* paceval_callbackStatus_in);
+     double pacevalLibrary_dGetComputationResult(void* handle_pacevalComputation_in, double values_in[],
+     double* trustedMinResult_out, double* trustedMaxResult_out);
+     bool pacevalLibrary_DeleteComputation(void* handle_pacevalComputation_in);
+     ",
+     "/usr/bin/libpacevalARM64_sharedLIB.so"); // you can exchange the ARM64-library, e.g. to the x64-library
 
 // call paceval-library functions to create a computation-object in memory, run a computation
 // with trusted intervals and delete the computation-object from memory when we are done
--- examples_sources/PHP_examples/example2.php
+++ examples_sources/PHP_examples/example2.php
@@ -3,18 +3,18 @@
 // PHP example 2
 
 $paceval_ffi = FFI::cdef("
-bool pacevalLibrary_Initialize(const char* initString_in);
-bool pacevalLibrary_Free();
-void* pacevalLibrary_CreateComputation(const char* functionString_in,
-unsigned long numberOfVariables_in, const char* variables_in, bool useInterval_in, void* paceval_callbackStatus_in);
-bool pacevalLibrary_dGetComputationResultExt(void* handle_pacevalComputation_in, double values_in[],
-unsigned long numberOfCalculations_in, double* results_out, double* trustedMinResults_out,
-double* trustedMaxResults_out, int* errorTypes_out);
-double pacevalLibrary_dmathv(void* handle_pacevalComputation_out_in, int *errorType_out,
-const char* functionString_in, unsigned long numberOfVariables_in, const char* variables_in, double values_in[]);
-bool pacevalLibrary_DeleteComputation(void* handle_pacevalComputation_in);
-",
-"/usr/bin/libpacevalARM64_sharedLIB.so");
+     bool pacevalLibrary_Initialize(const char* initString_in);
+     bool pacevalLibrary_Free();
+     void* pacevalLibrary_CreateComputation(const char* functionString_in,
+     unsigned long numberOfVariables_in, const char* variables_in, bool useInterval_in, void* paceval_callbackStatus_in);
+     bool pacevalLibrary_dGetComputationResultExt(void* handle_pacevalComputation_in, double values_in[],
+     unsigned long numberOfCalculations_in, double* results_out, double* trustedMinResults_out,
+     double* trustedMaxResults_out, int* errorTypes_out);
+     double pacevalLibrary_dmathv(void* handle_pacevalComputation_out_in, int *errorType_out,
+     const char* functionString_in, unsigned long numberOfVariables_in, const char* variables_in, double values_in[]);
+     bool pacevalLibrary_DeleteComputation(void* handle_pacevalComputation_in);
+     ",
+     "/usr/bin/libpacevalARM64_sharedLIB.so");
 
 // call paceval-library functions to create a computation-object in memory,
 // run 1.000.000 computations for x in [-10, 10] and y set to 0.5 without trusted interval computing
@@ -27,8 +27,7 @@ $errorTypes = FFI::new("int[1000000]");
 $delta_x = 20.0/1000000;
 $errorType = FFI::new("int");
 
-$handle_pacevalComputation = $paceval_ffi->pacevalLibrary_CreateComputation("(sin(x)*cos(y)*x*y)+(x/((x^2+y^2)))",
-2, "x y", false, null);
+$handle_pacevalComputation = $paceval_ffi->pacevalLibrary_CreateComputation("(sin(x)*cos(y)*x*y)+(x/((x^2+y^2)))", 2, "x y", false, null);
 
 for($iCount = 0; $iCount < 1000000; $iCount = $iCount + 1)
 {
@@ -45,8 +44,7 @@ var_dump($results[999999]); //-2.287287969116
 
 $success = (bool) $paceval_ffi->pacevalLibrary_DeleteComputation($handle_pacevalComputation);
 
-$threadUsages = $paceval_ffi->pacevalLibrary_dmathv(null, FFI::addr($errorType), "paceval_NumberThreadUsages",
-0, "", null);
+$threadUsages = $paceval_ffi->pacevalLibrary_dmathv(null, FFI::addr($errorType), "paceval_NumberThreadUsages", 0, "", null);
 var_dump($threadUsages); //e.g. 12 threads
 
 $success = (bool) $paceval_ffi->pacevalLibrary_Free();
