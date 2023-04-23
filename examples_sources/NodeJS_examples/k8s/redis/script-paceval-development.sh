5953e57102217b0bc8de3089316bb360ece606f8
paceval <info@paceval.com>
2023-04-23 12:25:24 +0200

    Update Redis script.sh

--- examples_sources/NodeJS_examples/k8s/redis/script-paceval-development.sh
+++
@@ -1,3 +0,0 @@
-helm install redis bitnami/redis -n redis --create-namespace --set auth.enabled=false
-
-helm template redis bitnami/redis -n redis --create-namespace --set auth.enabled=false > redis-template.yaml
