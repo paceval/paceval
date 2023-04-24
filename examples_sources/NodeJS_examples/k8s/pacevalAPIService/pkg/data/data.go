07e6dec8db38b58a2c1821d46321a782ef06bea3
ricogu <ricogu@gmail.com>
2023-04-24 19:10:00 +0200

    use declare type to match demoService output instead of map in order to preserve order

--- examples_sources/NodeJS_examples/k8s/pacevalAPIService/pkg/data/data.go
+++ examples_sources/NodeJS_examples/k8s/pacevalAPIService/pkg/data/data.go
@@ -29,6 +29,19 @@ type DemoParameterSet struct {
 	Values string `json:"values,omitempty"`
 }
 
+type DemoServiceResponse struct {
+	FunctionId    string  `json:"handle_pacevalComputation,omitempty"`
+	Result        string  `json:"result,omitempty"`
+	IntervalMin   string  `json:"interval-min-result,omitempty"`
+	IntervalMax   string  `json:"interval-max-result,omitempty"`
+	ErrorTypeNum  int     `json:"error-type-number,omitempty"`
+	ErrorPosition string  `json:"error-position,omitempty"`
+	ErrorType     string  `json:"error-type,omitempty"`
+	ErrorMessage  string  `json:"error-message,omitempty"`
+	TimeCalculate string  `json:"time-calculate,omitempty"`
+	Version       float32 `json:"version-number,omitempty"`
+}
+
 type HandlePacevalComputationObject struct {
 	HandleCreateComputation string `json:"handle_pacevalComputation,omitempty"`
 }
