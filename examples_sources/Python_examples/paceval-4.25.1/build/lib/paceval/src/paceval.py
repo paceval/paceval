629d9f40fe7619c1ee13ed428899a5600c3720cc
paceval. <info@paceval.com>
2024-10-07 04:17:22 +0200

    Updated Python package

---
+++ examples_sources/Python_examples/paceval-4.25.1/build/lib/paceval/src/paceval.py
@@ -0,0 +1,128 @@
+#---------------------------------------------------------------------------
+# Copyright ©1994-2024. Version 1.x, 2.x, 3.x, 4.x ©1994-2024
+#       Joerg Koenning & paceval.[Registered Trade Mark] All rights reserved.
+# Author(s) : paceval., see http://www.paceval.com
+# Reference: https://app.swaggerhub.com/apis/paceval/paceval-service
+# Documentation: https://pypi.org/project/paceval/
+#---------------------------------------------------------------------------
+
+import requests
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_Demo_
+def Demo(mainUrl, functionString, numberOfVariables, variables, values, interval):
+
+    url = mainUrl + "/Demo/"
+    payload = {
+        "functionString": functionString,
+        "numberOfVariables": numberOfVariables,
+        "variables": variables,
+        "values": values,
+        "interval": interval
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_CreateComputation_
+def CreateComputation(mainUrl, functionString, numberOfVariables, variables, interval):
+
+    url = mainUrl + "/CreateComputation/"
+    payload = {
+        "functionString": functionString,
+        "numberOfVariables": numberOfVariables,
+        "variables": variables,
+        "interval": interval
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_GetComputationResult_
+def GetComputationResult(mainUrl, handle_pacevalComputation, values):
+
+    url = mainUrl + "/GetComputationResult/"
+    payload = {
+        "handle_pacevalComputation": handle_pacevalComputation,
+        "values": values
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_GetErrorInformation_
+def GetErrorInformation(mainUrl, handle_pacevalComputation):
+
+    url = mainUrl + "/GetErrorInformation/"
+    payload = {
+        "handle_pacevalComputation": handle_pacevalComputation
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_GetComputationResultExt_
+def GetComputationResultExt(mainUrl, handle_pacevalComputation, numberOfCalculations, values):
+
+    url = mainUrl + "/GetComputationResultExt/"
+    payload = {
+        "handle_pacevalComputation": handle_pacevalComputation,
+        "numberOfCalculations": numberOfCalculations,
+        "values": values
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_GetMultipleComputationsResults_
+def GetMultipleComputationsResults(mainUrl, handle_pacevalComputations, numberOfpacevalComputations, values):
+
+    url = mainUrl + "/GetMultipleComputationsResults/"
+    payload = {
+        "handle_pacevalComputations": handle_pacevalComputations,
+        "numberOfpacevalComputations": numberOfpacevalComputations,
+        "values": values
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_GetMultipleComputationsResultsExt
+def GetMultipleComputationsResultsExt(mainUrl, handle_pacevalComputations, numberOfpacevalComputations, numberOfCalculations, values):
+
+    url = mainUrl + "/GetMultipleComputationsResultsExt/"
+    payload = {
+        "handle_pacevalComputations": handle_pacevalComputations,
+        "numberOfpacevalComputations": numberOfpacevalComputations,
+        "numberOfCalculations": numberOfCalculations,
+        "values": values
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
+
+
+# see https://app.swaggerhub.com/apis/paceval/paceval-service/4.25#/default/post_GetComputationInformationXML_
+def GetComputationInformationXML(mainUrl, handle_pacevalComputation):
+
+    url = mainUrl + "/GetComputationInformationXML/"
+    payload = {
+        "handle_pacevalComputation": handle_pacevalComputation
+    }
+ 
+    response = requests.request("POST", url, data=payload)
+ 
+    return response
